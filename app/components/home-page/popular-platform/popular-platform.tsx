f9ceea41f0621e5a2ef18ac3077f56e423c624b6
Shoriful Habib <shorifulhabib.iit@gmail.com>
2025-08-10 01:34:40 +0600

    feat: popular platform section added

--- app/components/home-page/popular-platform/popular-platform.tsx
+++ app/components/home-page/popular-platform/popular-platform.tsx
@@ -1,4 +1,5 @@
 import { AppImages } from "@/app/shared/image/image";
+import Image from "next/image";
 import React from "react";
 
 const PopularPlatform = () => {
@@ -54,15 +55,31 @@ const PopularPlatform = () => {
   ];
   return (
     <section className="py-[50px] md:pt-[100px] md:pb-[50px]">
-      <div className="px-[15px] mx-auto max-w-[1200px]">
-        <section className="max-w-[600px] mx-auto text-center">
-          <h2 className="text-secondary font-bold text-[25px] leading-[30px] mb-[10px] md:text-[40px] md:leading-12">
-            Easy integration <br /> with popular platforms
-          </h2>
-          <p className="text-gray text-[16px] leading-[27px] md:text-[17px] md:leading-[29px] font-normal mb-4">
-            Lorem Ipsum is simply dummy text of the printing and typese tting
-            indus orem Ipsum has beenthe standard dummy text ever since.
-          </p>
+      <div className=" mx-auto max-w-[1200px]">
+        <div className="px-[15px]">
+          <section className="max-w-[600px] mx-auto text-center">
+            <h2 className="text-secondary font-bold text-[25px] leading-[30px] mb-[10px] md:text-[40px] md:leading-12">
+              Easy integration <br /> with popular platforms
+            </h2>
+            <p className="text-gray text-[16px] leading-[27px] md:text-[17px] md:leading-[29px] font-normal mb-4">
+              Lorem Ipsum is simply dummy text of the printing and typese tting
+              indus orem Ipsum has beenthe standard dummy text ever since.
+            </p>
+          </section>
+        </div>
+        <section className="grid grid-cols-2  md:grid-cols-4  lg:grid-cols-6">
+          {cardData.map((card, key) => (
+            <div key={key} className="px-[15px] min-w-50">
+              <div className="bg-white mt-[30px] border-x border-t border-b-4 p-[10px] border-pale-blue flex flex-col items-center shadow-[0_3px_15px] shadow-[#edebf6] transition-all duration-400 hover:border-b-primary  rounded-md ">
+                <div className="h-25 flex items-center">
+                  <Image src={card.image} alt="card-image"></Image>
+                </div>
+                <h2 className="text-gray text-[16px] leading-[27px]  md:text-[17px] md:leading-[29px] font-semibold mb-4">
+                  {card.label}
+                </h2>
+              </div>
+            </div>
+          ))}
         </section>
       </div>
     </section>
